8783dddbd267376bea6671ace7404136558e9b26
SopheaktraKong <trasopheak9091@gmail.com>
2024-03-01 11:12:05 +0700

    delete title

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import type { PropsWithChildren } from 'react';
+import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -30,7 +30,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({ children, title }: SectionProps): React.JSX.Element {
+function Section({children, title}: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -40,9 +40,7 @@ function Section({ children, title }: SectionProps): React.JSX.Element {
           {
             color: isDarkMode ? Colors.white : Colors.black,
           },
-        ]}>
-        {title}
-      </Text>
+        ]}></Text>
       <Text
         style={[
           styles.sectionDescription,
@@ -63,8 +61,7 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  return <HomeScreen />
-
+  return <HomeScreen />;
 }
 
 const styles = StyleSheet.create({
